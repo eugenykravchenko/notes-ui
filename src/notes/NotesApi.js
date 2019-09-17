4982b1967991fcc9178f2ff4b156eece74efd999
Eugeny Kravchenko <e.kravchenko.end@gmail.com>
2019-09-17 20:32:11 +0300

    removed redundant logging

--- src/notes/NotesApi.js
+++ src/notes/NotesApi.js
@@ -1,7 +1,6 @@
 import config from '../config';
 
 export async function getNotes() {
-    console.log(config);
     const response = await fetch(config.apiUrl + '/notes');
     return response.json();
 }
